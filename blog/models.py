d9f56703f0b943090a7397a3b96b77b65ea17b35
gray.pro <gray.aloner@icloud.com>
2021-01-24 17:52:26 +0100

    finish mublog

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=300)
-	date = models.DateTimeField()
-	text = models.TextField()
 	image = models.ImageField(upload_to='event_images/')
+	text = models.TextField()
+	date = models.DateTimeField()
 	
+	def get_summery(self):
+		return self.text[:56]
+
+
+	def __str__(self):
+		return self.title
+
+
+
---
+++ contacts/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class my_contacts(models.Model):
+	text = models.TextField()
